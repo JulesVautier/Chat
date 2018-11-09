745aed6320328ce606a5a97ecfe8610fed972e70
jules vautier <jules.vautier@epitech.eu>
2018-11-09 21:24:46 +0100

    remove auth and friends

--- model/model.js
+++ model/model.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-mongoose.connect("mongodb://localhost:27017/chat");
 
 mongoose.connection.on('open', function (ref) {
     console.log('Connected to mongo server.');
@@ -12,16 +11,9 @@ mongoose.connection.on('error', function (err) {
     console.log(err);
 });
 
+// mongoose.connect("mongodb://localhost:27017/chat");
 mongoose.connect('mongodb://localhost/mongodb');
 
-module.exports.user=mongoose.model('User',new Schema({
-    name:String,
-    handle: String,
-    password: String,
-    phone:String,
-    email:String,
-    friends:[]
-},{strict: false}));
 module.exports.online=mongoose.model('online',new Schema({
     handle:String,
     connection_id:String
